0179645ddefb5c775826479507b411e78214122e
Larry Tremblay <lrt512@gmail.com>
2025-07-26 23:15:48 -0500

    Let self-serve updates actually edit info

--- emol/cards/templatetags/permissions.py
+++ emol/cards/templatetags/permissions.py
@@ -1,16 +1,41 @@
+import logging
+
 from cards.models.user_permission import UserPermission
 from current_user import get_current_user
 from django import template
 
+logger = logging.getLogger("cards")
 register = template.Library()
 
 
 @register.simple_tag
-def has_global_permission(user, permission_slug):
+def has_global_permission(user, permission_slug, override=None, reason=None):
+    """Check a global permission.
+
+    Args:
+        user: The user to check the permission for
+        permission_slug: The slug of the permission to check
+        override: If True, the permission will be overridden by the extra permission
+
+        Currently, override is only used for self-serve updates to allow
+        combatants to update their own information.
+
+    Returns:
+        True if the user has the permission, False otherwise
+    """
+    if override:
+        if not reason:
+            logger.error(f"has_global_permission: {permission_slug} override: {override} but no reason provided")
+            return False
+
+        logger.info(f"has_global_permission: {permission_slug} override: {override} reason: {reason}")
+        return override
+
     if not user.is_authenticated:
         return False
 
-    return UserPermission.user_has_permission(user, permission_slug)
+    permission = UserPermission.user_has_permission(user, permission_slug)
+    return permission
 
 
 @register.simple_tag
